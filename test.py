0ad3c7f01c6ca9e4144ee76a0ee9de6f25fa35c2
farza <farzanrichu610@gmail.com>
2025-11-28 00:53:44 +0530

    rewrote the code with mediapipe ->efficency improved

--- test.py
+++ test.py
@@ -1,44 +1,51 @@
-import matplotlib
-matplotlib.use("TkAgg")
-
-from retinaface import RetinaFace
 import cv2
-import matplotlib.pyplot as plt
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 
+model_path = "models//blaze_face_short_range.tflite"
+BaseOptions = mp.tasks.BaseOptions
+FaceDetector = mp.tasks.vision.FaceDetector
+FaceDetectorOptions = mp.tasks.vision.FaceDetectorOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+options = FaceDetectorOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.VIDEO,
+    min_detection_confidence=0.5
+)
+
+detector =  FaceDetector.create_from_options(options)
 
 cam = cv2.VideoCapture(0)
+
 if not cam.isOpened():
     print("Cannot open camera")
     exit()
 
-
 while True:
-    ret, frame = cam.read()
+    ret,frame=cam.read()
     if not ret:
-        print("Error: Failed to read frame.")
-        break
+        print("Cannot read frame")
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+    detection_result = detector.detect_for_video(mp_image, timestamp_ms=int(cam.get(cv2.CAP_PROP_POS_MSEC)))
 
-
-    try:
-        resp=RetinaFace.detect_faces(frame)
-        if not resp:
-            print("Error: Failed to detect faces.")
-    except Exception as e:
-        print("Detection error:", e)
-        resp = None
-
-    if isinstance(resp, dict):
-        for key in resp:
-            face = resp[key]
-            x1, y1, x2, y2 = face["facial_area"]
+    if detection_result.detections:
+        for detection in detection_result.detections:
+            bbox = detection.bounding_box
+            x1 = int(bbox.origin_x)
+            y1 = int(bbox.origin_y)
+            x2 = int(bbox.origin_x + bbox.width)
+            y2 = int(bbox.origin_y + bbox.height)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-    cv2.imshow('Webcam Feed', frame)
+    cv2.imshow("BlazeFace Webcam", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
 
 cam.release()
 cv2.destroyAllWindows()
